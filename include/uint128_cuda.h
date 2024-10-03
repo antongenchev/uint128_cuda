d4264730eabcaa31d018cfaad6127caa273c1652
antongenchev <antongenchevfractal@gmail.com>
2024-10-03 20:51:40 +0300

    Add googletest, Add CmakeLists.txt file, add .vscode configuration, overide addition for uint128_t

--- include/uint128_cuda.h
+++ include/uint128_cuda.h
@@ -8,8 +8,17 @@ struct uint128_t {
     uint64_t high;
     uint64_t low;
 
+    // Constructors
     __host__ __device__ uint128_t() : high(0), low(0) {}
     __host__ __device__ uint128_t(uint64_t high, uint64_t low) : high(high), low(low) {}
+
+    // Operator overloading: +
+    __host__ __device__ uint128_t operator+(const uint128_t& other) const {
+        uint128_t result;
+        result.low = low + other.low;
+        result.high = high + other.high + (result.low < other.low ? 1 : 0);
+        return result;
+    }
 };
 
 
