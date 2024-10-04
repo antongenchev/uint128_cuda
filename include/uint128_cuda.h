0425cc704f49cb42c1386504a3394a1a8b703295
antongenchev <antongenchevfractal@gmail.com>
2024-10-04 20:12:01 +0300

    Add multiplication of 128 bit integers. Add uint128_cuda_utils.h file. Write tests
    -Add a function multiply_uint64_t in a new file uint128_cuda_utils which is used to calculate the product of two 64 bit unsigned ints
    -Overload the multiplicaton operator for uint128_t. The multiplication is correct modulo 2^128
    -Implement edge case tests for the multiplication

--- include/uint128_cuda.h
+++ include/uint128_cuda.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <cuda_runtime.h>
+#include <uint128_cuda_utils.h>
 
 struct uint128_t {
     uint64_t high;
@@ -19,7 +20,15 @@ struct uint128_t {
         result.high = high + other.high + (result.low < other.low ? 1 : 0);
         return result;
     }
-};
 
+    // Operator overloading: *
+    __host__  uint128_t operator*(const uint128_t& other) const {
+        // (a*2^64 + b)(c*2^64 + d) % 2^128 = (a*d + a*c)*2^64 + b*d % 2^128
+        uint128_t result;
+        multiply_uint64_t(low, other.low, &result.high, &result.low);
+        result.high += low * other.high + high * other.low;
+        return result;
+    }
+};
 
 #endif // UINT128
